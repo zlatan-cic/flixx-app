ccb99fe9f74962aa5a7fee13beaeaaa167697ebe
zlatan-cic <smajicz999@gmail.com>
2023-07-25 23:18:59 +0200

    Search Function custom alert and display search results.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const global = {
   curentPage: window.location.pathname,
+  search: {
+    term: "",
+    type: "",
+    page: 1,
+    totalPages: 1,
+  },
 };
 
 // show 20 popular movies
@@ -67,7 +73,9 @@ async function displayPopularTVShow() {
       <div class="card-body">
         <h5 class="card-title">${show.name}</h5>
         <p class="card-text">
-          <small class="text-muted">Air Date: ${show.first_air_day}</small>
+          <small class="text-muted">First Air Date: ${
+            show.first_air_date
+          }</small>
         </p>
       </div>`;
 
@@ -154,7 +162,7 @@ async function displayTvShowDetails() {
   const showId = window.location.search.split("=")[1]; // 298618
 
   const show = await fetchAPIData(`tv/${showId}`);
-  // console.log(show);
+  console.log(show);
 
   // Overlay for background image
   displaybackgroundImage("tv", show.backdrop_path);
@@ -186,7 +194,10 @@ async function displayTvShowDetails() {
         <i class="fas fa-star text-primary"></i>
         ${show.vote_average.toFixed(1)} / 10
       </p>
-      <p class="text-muted">Release Date: ${show.last_air_date}</p>
+      <p class="text-muted">First Air Date: ${show.first_air_date}</p>
+      <p>
+      </p>
+      <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
       <p>
         ${show.overview}
       </p>
@@ -242,13 +253,84 @@ function displaybackgroundImage(type, backgroundPath) {
   }
 }
 
+// Search movie/show
+
+// Search Movies/Shows
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  global.search.type = urlParams.get("type");
+  global.search.term = urlParams.get("search-term");
+
+  if (global.search.term !== "" && global.search.term !== null) {
+    const { results, total_pages, page } = await searchAPIData();
+
+    if (results.length === 0) {
+      showAlert("No results found");
+      return;
+    }
+
+    displaySearchResults(results);
+
+    document.querySelector("#search-term").value = "";
+
+    console.log(results);
+  } else {
+    showAlert("Please enter a search term");
+  }
+}
+
+  function displaySearchResults(results) {
+    results.forEach((result) => {
+      const div = document.createElement("div");
+      div.classList.add("card");
+      div.innerHTML = `
+            <a href="${global.search.type}-details.html?id=${result.id}">
+              ${
+                result.poster_path
+                  ? `<img
+                src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+                class="card-img-top"
+                alt="${
+                  global.search.type === "movie" ? result.title : result.name
+                }"
+              />`
+                  : `<img
+              src="../images/no-image.jpg"
+              class="card-img-top"
+               alt="${
+                 global.search.type === "movie" ? result.title : result.name
+               }"
+            />`
+              }
+            </a>
+            <div class="card-body">
+              <h5 class="card-title">${
+                global.search.type === "movie" ? result.title : result.name
+              }</h5>
+              <p class="card-text">
+                <small class="text-muted">Release: ${
+                  global.search.type === "movie"
+                    ? result.release_date
+                    : result.first_air_date
+                }</small>
+              </p>
+            </div>
+          `;
+
+      document.querySelector("#search-results").appendChild(div);
+    });
+  }
+
+
 // Display Slider Movies
 async function displaySlider() {
-  const { results } = await fetchAPIData('movie/now_playing');
+  const { results } = await fetchAPIData("movie/now_playing");
 
   results.forEach((movie) => {
-    const div = document.createElement('div');
-    div.classList.add('swiper-slide');
+    const div = document.createElement("div");
+    div.classList.add("swiper-slide");
 
     div.innerHTML = `
       <a href="movie-details.html?id=${movie.id}">
@@ -259,14 +341,14 @@ async function displaySlider() {
       </h4>
     `;
 
-    document.querySelector('.swiper-wrapper').appendChild(div);
+    document.querySelector(".swiper-wrapper").appendChild(div);
 
     initSwiper();
   });
 }
 
 function initSwiper() {
-  const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper(".swiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     freeMode: true,
@@ -291,6 +373,7 @@ function initSwiper() {
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
+  // Register your key at https://www.themoviedb.org/settings {to get api_key for free}
   const API_KEY = "974d1202c80148fde9e6f7c2b36bc47e";
   const API_URL = "https://api.themoviedb.org/3/";
 
@@ -299,6 +382,36 @@ async function fetchAPIData(endpoint) {
   try {
     const response = await fetch(
       `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+    );
+
+    // Check if the response has a successful status (2xx)
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+
+    const data = await response.json();
+
+    hideSpinner();
+
+    return data;
+  } catch (error) {
+    hideSpinner();
+    console.error("Error fetching data:", error);
+    // Handle the error gracefully, e.g., display an error message to the user
+  }
+}
+
+// Make Request to Search
+async function searchAPIData() {
+  // Register your key at https://www.themoviedb.org/settings/api
+  const API_KEY = "974d1202c80148fde9e6f7c2b36bc47e";
+  const API_URL = "https://api.themoviedb.org/3/";
+
+  showSpinner();
+
+  try {
+    const response = await fetch(
+      `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
     );
 
     // Check if the response has a successful status (2xx)
@@ -342,6 +455,16 @@ function numberWithCommas(num) {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+// Show Alert
+function showAlert(message, className = "error") {
+  const alertEl = document.createElement("div");
+  alertEl.classList.add("alert", className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector("#alert").appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
+}
+
 // Init App
 function init() {
   switch (global.curentPage) {
@@ -365,6 +488,7 @@ function init() {
       break;
     case "/search.html":
       console.log("Search");
+      search();
       break;
   }
 
