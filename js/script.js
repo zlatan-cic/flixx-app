ad21d7e99d9eef56268e9ebf2dc3a78ab26e2fe5
zlatan-cic <smajicz999@gmail.com>
2023-07-25 15:55:16 +0200

    Implement feature to display popular movies on the homepage.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,53 @@ const global = {
   curentPage: window.location.pathname,
 };
 
+async function displayPopularMovies(){
+  // "*""Curly braces {results} are used, and then we have a straight array.""*"
+  const {results} = await fetchAPIData('/movie/popular');
+  // console.log(results);
+
+  results.forEach((movie) => {
+    const movieEl = document.createElement('div');
+    movieEl.classList.add("card");
+    movieEl.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        ${
+          movie.poster_path
+            ? `<img
+            src="http://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+          />` : `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="${movie.title}"
+        />`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${movie.title}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release: ${movie.release_date}</small>
+        </p>
+      </div>`;
+
+      document.querySelector('#popular-movies').appendChild(movieEl)
+  })
+
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = "974d1202c80148fde9e6f7c2b36bc47e";
+  const API_URL = "https://api.themoviedb.org/3";
+
+  const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+
+  const data = await response.json();
+
+  return data;
+}
+
 // Highlight active link
 function highlightActiveLink() {
   const links = document.querySelectorAll(".nav-link")
@@ -13,14 +60,13 @@ function highlightActiveLink() {
   })
 }
 
-
-
 // Init App
 function init() {
   switch (global.curentPage) {
     case "/":
     case "/index.html":
       console.log("Home");
+      displayPopularMovies()
       break;
     case "/shows.html":
       console.log("Shows");
@@ -39,6 +85,7 @@ function init() {
 
   // functions calling
   highlightActiveLink()
+  
 }
 
 document.addEventListener("DOMContentLoaded", init);
